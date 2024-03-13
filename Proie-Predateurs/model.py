9351bbb0ca8a86df06a3ccb9d95a53817290d8a7
Jhonatan Felix <jhonyrf@gmail.com>
2024-03-13 16:51:24 +0100

    First simple simulation

--- Proie-Predateurs/model.py
+++ Proie-Predateurs/model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import random
-
+import tkinter as tk
 
 # ================ Fonctions pour utiliser ==================================
 
@@ -27,7 +27,7 @@ def function_detecter_blaireau(valeur):
         return math.exp(-(valeur - 1)/2)
 
 
-def function_detecter_predateur():
+def function_detecter_proie():  # mudar saporra para ficar com =1 no intervalo muito perto
     return 0.5
 
 
@@ -36,7 +36,6 @@ def function_detecter_predateur():
 class Animal:
     def __init__(self, position):
         self.position = np.array(position)
-        self.direction = np.array([0, 0])
         self.captured_prey = None
 
     def move(self, vector):
@@ -47,69 +46,133 @@ class Animal:
 # ========================= Proie =====================================================
 
 class Proie(Animal):
+
     def __init__(self, position):
         super().__init__(position)
 
         self.identified_predator = False
         # Tenho que fazer a condição para mudar isso dentro da simulação
         self.identified_haie = False
+        self.is_in_haie = False
+
+        self.s_predateurs = []
+        self.haie_plus_proche = None
+
+    def detecter_haie(self, haies):
+        if self.is_in_haie == False and self.identified_haie == False:
+            distances = np.array([math.dist(obstaculo.position, self.position)
+                                  for obstaculo in haies])
+            proches = distances[distances < 6]
+            array_haies = np.array(haies)
+            array_haies = array_haies[proches]
+
+            if len(array_haies) > 0:
+                self.identified_haie = True
+            else:
+                self.identified_haie = False
 
     def find_haie_plus_proche(self, haies):
         '''Essa função precisa receber o champ.obstacles como arbustos para funcionar
         pois ela vai pegar a lista de arbustos, medir a distância, selecionar o arbusto
         mais próximo.'''
+        if self.is_in_haie == False and self.identified_predator == False:
+            if haies != []:
+                distances = [math.dist(obstaculo.position, self.position)
+                             for obstaculo in haies]
+                premier = distances.index(min(distances))
 
-        distances = [math.dist(obstaculo.position, self.position)
-                     for obstaculo in haies]
-        premier = distances.index(min(distances))
+                self.haie_plus_proche = haies[premier]
+            else:
+                self.haie_plus_proche = None
+        else:
+            pass
 
-        return haies[premier]
-
-    def marcher_haie(self, haie):
+    def marcher_haie(self, haie=None):
         '''Ideia melhor: primeiro dentro do jogo vamos verificar se existe tal arbusto,
         se sim vamos poder andar em direção a ele. Portanto, essa função vai receber o
         arbusto já sabendo que ele está nas condições que eu quero. E sendo assim a minha
         presa vai poder andar em direção a ele.'''
-
-        if self.position[0] != haie.position[0]:
-            self.move([1, 0])
-        elif self.position[1] != haie.position[1]:
-            self.move([0, 1])
-
+        if haie != None:
+            if self.position[0] != haie.position[0]:
+                if self.position[0] > haie.position[0]:
+                    self.move([1, 0])
+                else:
+                    self.move([-1, 0])
+            elif self.position[1] != haie.position[1]:
+                if self.position[1] > haie.position[1]:
+                    self.move([0, 1])
+                else:
+                    self.move([0, -1])
+            else:
+                self.is_in_haie = True
+        else:
+            pass
 
     def detecter_predateur(self, list_predators):
+        array_predators = np.array(list_predators)
         distances = [math.dist(self.position, predateur.position)
                      for predateur in list_predators]
         filtered_list = np.array(distances) < 8
 
         for i, valeur in enumerate(filtered_list):
             if valeur == 1:
-                probabilite = function_detecter_predateur()
+                probabilite = function_detecter_proie()
                 if random.random() < probabilite:
                     filtered_list[i] = 0
 
         if len(filtered_list[filtered_list]) >= 1:
             self.identified_predator = True
-        
+            self.s_predateurs = array_predators[filtered_list]
 
-    def senfuire(self, predateur):
+    def senfuire(self, predateur, champ):
+        '''Tenho que mudar essa função, pois vão existir lugares no mapa onde la proie não
+        vai poder fugir, então, eu tenho que sempre considerar esses pontos.'''
+
         '''De novo, calcula a presença de um predador próximo, e se estiver dentro de uma
         distância mínima n então a presa foge, mas só se ela estiver fora do arbusto. A 
         presa vai calcular a distância máxima que ela pode ficar de um predador e correr
         exatamente para esse ponto.'''
-        positions = []
-        for i in range(-3, 4):
-            positions.append(self.position + np.array([i, 0]))
-        for i in range(-3, 4):
-            positions.append(self.position + np.array([0, i]))
+        if True:
+            if self.identified_predator == True:
+                if self.is_in_haie == False or predateur.position == self.position:
+                    positions = []
+                    for i in range(-3, 4):
+                        positions.append(self.position + np.array([i, 0]))
+                    for i in range(-3, 4):
+                        positions.append(self.position + np.array([0, i]))
 
-        # Calculando a mínima e para onde correr
-        distances = [math.dist(possibilities, predateur.position)
-                     for possibilities in positions]
-        premier = distances.index(max(distances))
+                    positions = np.array(positions)
+                    positions = positions[positions[:, 0] < champ.width]
+                    positions = positions[positions[:, 1] < champ.height]
+                    positions = positions[positions[:, 0] > 0]
+                    positions = positions[positions[:, 1] > 0]
 
-        # Aqui temos a locomoção da presa para onde ela fugiu
-        self.position = positions[premier]
+                    # Calculando a mínima e para onde correr
+                    distances = [math.dist(possibilities, predateur.position)
+                                 for possibilities in positions]
+                    premier = distances.index(max(distances))
+
+                    # Aqui temos a locomoção da presa para onde ela fugiu
+                    self.position = positions[premier]
+                    self.is_in_haie = False
+        else:
+            pass
+
+    def update_state(self, champ):
+        self.detecter_predateur(champ.predateurs)
+        if self.identified_predator == True:
+            if self.is_in_haie == True:
+                if np.array([predateur.position for predateur in champ.predateurs]).any() == self.position:
+                    self.senfuire(self.s_predateurs[0], champ)
+                else:
+                    pass
+            else:
+                self.senfuire(self.s_predateurs[0], champ)
+        else:
+            if self.identified_haie == True:
+                self.marcher_haie(self.haie_plus_proche)
+            else:
+                self.find_haie_plus_proche(champ.obstacles)
 
 
 # =========================== Predateur =================================================
@@ -118,34 +181,63 @@ class Predateur(Animal):
     def __init__(self, position):
         super().__init__(position)
         self.is_satiated = False
-        self.satiated_turns_remaining = 0
         self.assisting_other = False
         self.without_meat = 0
+        self.s_proie = None
 
         self.identified_prey = False
 
-    def detecter(self, proies):
+    def detecter(self, list_proies):
         ''' Essa função deve receber a lista de presas, verificar se alguma foi identificada
         de acordo com uma função de (quase)probabilidade pré-determinada e com isso verificar
         a mais próxima e passar para a função de caça.'''
-        pass
+        array_proies = np.array(list_proies)
+        distances = [math.dist(self.position, proie.position)
+                     for proie in list_proies]
+        filtered_list = np.array(distances) < 15
 
-    def hunt(self, prey):
+        for i, valeur in enumerate(filtered_list):
+            if valeur == 1:
+                probabilite = function_detecter_coyote(distances[i])
+                if random.random() < probabilite:
+                    filtered_list[i] = 0
+
+        if len(filtered_list[filtered_list]) >= 1:
+            self.identified_prey = True
+            self.s_proie = array_proies[filtered_list][0]
+
+    def hunt(self, prey, champ):
         '''Essa função vai receber uma presa para caçar, e ela vai precisar avaliar os possíveis
         caminhos para poder alcançar essa presa.'''
-        possibilities = []
-        for i in range(-5, 6):
-            possibilities.append(self.position + np.array([i, 0]))
-        for i in range(-5, 6):
-            possibilities.append(self.position + np.array([0, i]))
-        for i in range(-5, 6):
-            possibilities.append(self.position + np.array([i, i]))
+        if prey != None:
+            if self.identified_prey == True:
+                possibilities = []
 
-        distances = [math.dist(positions, prey.position)
-                     for positions in possibilities]
-        premier = distances.index(min(distances))
-        self.position = positions[premier]
+                for i in range(-5, 6):
+                    possibilities.append(self.position + np.array([i, 0]))
+                for i in range(-5, 6):
+                    possibilities.append(self.position + np.array([0, i]))
+                for i in range(-5, 6):
+                    possibilities.append(self.position + np.array([i, i]))
 
+                possibilities = np.array(possibilities)
+                possibilities = possibilities[possibilities[:, 0]
+                                              < champ.width]
+                possibilities = possibilities[possibilities[:, 1]
+                                              < champ.height]
+                possibilities = possibilities[possibilities[:, 0]
+                                              > 0]
+                possibilities = possibilities[possibilities[:, 1]
+                                              > 0]
+
+                distances = [math.dist(positions, prey.position)
+                             for positions in possibilities]
+                premier = distances.index(min(distances))
+                self.position = possibilities[premier]
+            else:
+                pass
+        else:
+            pass
 
     def is_dead(self, prey, list_preys):
         '''Essa é para verificar se matou uma presa e muda o status do predador. Recebe a 
@@ -153,20 +245,39 @@ class Predateur(Animal):
         presa específcia depois que ela morrer.
         A condição de posições iguais tem que estar necessariamente antes de começar o
         código para poder fazer esass coisas.'''
-        list_preys.remove(prey)
-        self.is_satiated = True
-        self.satiated_turns_remaining = 2
-        self.without_meat = 0
+        if prey != None:
+            if self.position[0] == prey.position[0] and self.position[1] == prey.position[1]:
+                if prey in list_preys:
+                    list_preys.remove(prey)
+                self.is_satiated = True
+                self.without_meat = 0
+                self.identified_prey = False
+        else:
+            pass
 
-    def death(self, list_predators, days_wihout_eating):
+    def death(self, champ):
         ''' Essa funçaõ vai receber a lista de predadores para matar o animal depois
         de days_wihout_eating dias sem comer.'''
-        if self.without_meat >= days_wihout_eating:
-            list_predators.remove(self)
+        if self in champ.predateurs:
+            champ.predateurs.remove(self)
 
     def random_walk(self):
         pass
 
+    def update_state(self, champ):
+        self.without_meat += 1  # Incrementa a contagem de dias sem comer
+        if self.without_meat >= 25:
+            # O predador morre se estiver sem comida por 25 dias
+            self.death(champ)
+        elif self.without_meat > 2:
+            if self.identified_prey == False:
+                self.detecter(champ.proies)
+
+            self.hunt(self.s_proie, champ)  # Caça a presa identificada
+            # Verifica se conseguiu matar a presa
+            self.is_dead(self.s_proie, champ.proies)
+        else:
+            pass  # O predador fica parado se não estiver com fome
 
 # ========================== Animaux predateurs ===========================================
 
@@ -201,7 +312,7 @@ class Haie:
 
 
 class Champ:
-    def __init__(self, width, height):
+    def __init__(self, width=50, height=50):
         self.width = width
         self.height = height
         self.proies = []
@@ -221,11 +332,18 @@ class Champ:
     def pass_turn(self):
         self.turn += 1
 
+    def process_turn(self):
+        for predateur in self.predateurs:
+            predateur.update_state(self)
+        for proies in self.proies:
+            proies.update_state(self)
+        self.turn += 1
 
 # ================================Implementation ====================================
 
+
 # Créer un dictionnaire pour faire les probabilités
-df = pd.read_csv("probabilites.csv")
+df = pd.read_csv("Proie-Predateurs/probabilites.csv")
 probabilites = {}
 
 for index, row in df.iterrows():
@@ -238,17 +356,87 @@ for index, row in df.iterrows():
         probabilites[clé] = row[col]
 
 
-if __name__ == '__main__':
-    champ = Champ(10, 10)  # Initialize field with size N x M
+# ============================ Game ==========================================
+class GameBoard(tk.Tk):
+    def __init__(self, champ, cell_size=10):
+        super().__init__()
+        self.title("Prédateur-Proie Simulation")
 
-    # A lógica do predador de turnos necessários para mudar os status tem que ser
-    # mudada especificamente aqui.
+        self.champ = champ
+        self.rows = champ.height
+        self.cols = champ.width
+        self.cell_size = cell_size
 
-    # A lógica de que o predador fica parado depois que comer tem que ser adicionada
-    # aqui também.
-    # Add predators and prey
-    # Add hedges
+        self.label_turn = tk.Label(self, text=f"Turno: {self.champ.turn}")
+        self.label_turn.pack()
 
-    # Start simulation
-    while not champ.is_simulation_over():
-        champ.simulate_turn()
+        self.canvas = tk.Canvas(
+            self, height=self.rows * self.cell_size, width=self.cols * self.cell_size)
+        self.canvas.pack()
+
+        self.draw_grid()
+        self.update()  # Chama o método de atualização para iniciar a simulação
+
+    def draw_grid(self):
+        for i in range(self.rows):
+            self.canvas.create_line(
+                0, i * self.cell_size, self.cols * self.cell_size, i * self.cell_size)
+        for i in range(self.cols):
+            self.canvas.create_line(
+                i * self.cell_size, 0, i * self.cell_size, self.rows * self.cell_size)
+
+    def place_objects(self):
+        self.canvas.delete("all")  # Limpa o canvas antes de redesenhar
+        self.draw_grid()  # Redesenhar a grade pode ser opcional, dependendo do desejado
+        for predator in self.champ.predateurs:
+            self.draw_object(predator.position, 'red')
+
+        for prey in self.champ.proies:
+            self.draw_object(prey.position, 'blue')
+
+        for haie in self.champ.obstacles:
+            self.draw_object(haie.position, 'green')
+
+    def draw_object(self, position, color):
+        x, y = position
+        self.canvas.create_rectangle(x * self.cell_size, y * self.cell_size,
+                                     (x + 1) * self.cell_size, (y + 1) * self.cell_size, fill=color)
+
+    def update(self):
+        self.champ.process_turn()  # Processa um turno do jogo
+        self.place_objects()  # Atualiza a visualização com os novos estados
+        self.label_turn.config(text=f"Turno:{self.champ.turn}")
+        # Aguarda 1000ms (1 segundo) e chama update novamente
+        self.after(500, self.update)
+
+
+# ======================= Add Predateurs et Proies ================================
+
+def create_and_add_predateurs(champ, number_of_predateurs):
+    for _ in range(number_of_predateurs):
+        x = random.randint(0, champ.width - 1)
+        y = random.randint(0, champ.height - 1)
+
+        new_predator = Predateur(position=[x, y])
+
+        champ.add_predateurs(new_predator)
+
+
+def create_and_add_proies(champ, number_of_proies):
+    for _ in range(number_of_proies):
+        x = random.randint(0, champ.width - 1)
+        y = random.randint(0, champ.height - 1)
+
+        new_proie = Proie(position=[x, y])
+
+        champ.add_proies(new_proie)
+
+
+champ = Champ(50, 50)
+create_and_add_predateurs(champ, 10)
+create_and_add_proies(champ, 50)
+
+
+# Iniciar a simulação com uma interface gráfica
+game_board = GameBoard(champ)
+game_board.mainloop()
